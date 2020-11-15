d8a2d0c163aecc31026ffe5a8727ec4e3472d1e0
Josue Lopez <josueal1@uci.edu>
2020-11-15 03:12:42 +0000

    feat(): GET 404.html errorhandler

--- main.py
+++ main.py
@@ -79,6 +79,10 @@ def search_page():
     if request.method == "GET":
         return render_template("search.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 # if conditional that runs program
 # if the file is being excuted from it's main file source
