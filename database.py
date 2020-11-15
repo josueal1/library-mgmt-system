8673406b1c68f75d27660c3252484c133ea5375d
Josue Lopez <josueal1@uci.edu>
2020-11-15 00:17:14 +0000

    feat(): GET books by query

--- database.py
+++ database.py
@@ -1,10 +1,33 @@
 from replit import db
 
-def showDB():
-  for k,v in db.items():
-    print(k,v)
-    print()
+def getDB(q=None):
+  if q:
+    filtered_db_copy = {}
 
+    for k,info in db.items():
+      # every metadata in a book info
+      for md in info.values():
+        q = q.lower()
+        md = md.lower()
+
+        # mentioned at least once in any field
+        if (q in md) or (md in q):
+          filtered_db_copy[k] = info
+          # skip the rest of md, move to next book
+          break
+
+    return filtered_db_copy
+    
+  else:
+    return db
+    
 
 def delete(item:'str'):
-  del db[item]
+  bid_deleted = db.pop(str(item))
+  return bid_deleted
+
+
+def insert(isbn, metadata):
+  # separate isbn and metadata
+  db[isbn] = metadata
+  return metadata
--- main.py
+++ main.py
@@ -1,97 +1,84 @@
 # importting python "libraries"
 # other code we can reuse "frameworks"
-from flask import Flask, render_template, request, redirect, url_for
-from replit import db
+from flask import Flask, render_template, request
+from database import getDB, delete, insert
 import random
 
 # Create a flask app
 app = Flask(
-  # pass down name of py file implicitly  
-	__name__,
-  # Name of html file folder
-	template_folder='templates',
-  # Name of directory for static files
-	static_folder='static'
-)
+    # pass down name of py file implicitly
+    __name__,
+    # Name of html file folder
+    template_folder='templates',
+    # Name of directory for static files
+    static_folder='static')
+
 
 # What happens when the user visits the site
-# We'll serve an HTML page mgmt system  
+# We'll serve an HTML page mgmt system
 @app.route('/')
 def base_page():
-  # Sets the random number
-	random_num = random.randint(1, 100000)
+    # Sets the random number
+    random_num = random.randint(1, 100000)
 
-  # base_page function returns an HTML template
-  # the app.route 'listener'
-	return render_template(
-    # template file path, starting from folder  
-		'base.html',
-    # Sets the var random_number in the template
-		random_number=random_num  
-	)
+    # base_page function returns an HTML template
+    # the app.route 'listener'
+    return render_template(
+        # template file path, starting from folder
+        'base.html',
+        # Sets the var random_number in the template
+        random_number=random_num)
+
 
 @app.route("/books", methods=["GET", "POST"])
 # book_page function handles the HTTPs requests
 def book_page():
-  if request.method == "POST":
-    # grabbing info given from the submitted HTML form
-    isbn = request.form["isbn"]
-    title = request.form["title"]
-    author = request.form["author"]
-    edition = request.form["edition"]
-    publication = request.form["publication"]
+    if request.method == "POST":
+        # grabbing info given from the submitted HTML form
+        isbn = request.form["isbn"]
+        title = request.form["title"]
+        author = request.form["author"]
+        edition = request.form["edition"]
+        publication = request.form["publication"]
 
-    new_book = {
-      "isbn": isbn,
-      "title": title,
-      "author": author,
-      "edition": edition,
-      "publication": publication
-    }
+        new_book = {
+            "isbn": isbn,
+            "title": title,
+            "author": author,
+            "edition": edition,
+            "publication": publication
+        }
 
-    db[isbn] = new_book
+        insert(isbn, new_book)
 
-    return render_template(
-      "base.html",
-      book=new_book,
-      book_was_entered=True
-    )
-  
-  if request.method == "GET":
-    # query = request.form["search"]
-    # for k,v in db.items():
-    #   if (query in k) or (k in query)
-    return render_template(
-      "displayBooks.html",
-      db=db
-    )
-    
+        return render_template(
+            "base.html", book=new_book, book_was_entered=True)
+
+    if request.method == "GET":
+        query = request.args.get("search")
+        # query may be None, getDB handles
+        return render_template("displayBooks.html", db=getDB(query))
+
+
 @app.route("/api/v1/books", methods=["DELETE"])
 def delete_book():
-  bid = request.args.get("id")
-  if bid is None:
-    bid = ""
+    bid = request.args.get("id")
+    if bid is None:
+        bid = ""
 
-  if request.method == "DELETE":
-    bid_deleted = db.pop(str(bid))
-    return bid_deleted
-
+    if request.method == "DELETE":
+        return delete(bid)
 
 
-@app.route("/search", methods=["GET","POST"])
+@app.route("/search", methods=["GET", "POST"])
 def search_page():
-  if request.method == "GET":
-    return render_template("search.html")
-
+    if request.method == "GET":
+        return render_template("search.html")
 
 
 # if conditional that runs program
 # if the file is being excuted from it's main file source
 if __name__ == "__main__":
-  # then run the app on 0.0.0.0 on port:5000
-  # 127.0.0.1 is localhost on hardware computers
-  app.run(
-    debug=True,
-    host="0.0.0.0",
-    port=5000
-  )
+    # then run the app on 0.0.0.0 on port:5000
+    # 127.0.0.1 is localhost on hardware computers
+    app.run(debug=True, host="0.0.0.0", port=5000)
